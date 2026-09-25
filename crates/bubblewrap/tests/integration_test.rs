d620836c89ee2f9d9f20ffa65e6f36f03baf21d2
swkima-popos <greyonth@gmail.com>
2026-09-25 23:59:41 +0900

    feat: implemented sinple user namespace and added an integration test

---
+++ crates/bubblewrap/tests/integration_test.rs
@@ -0,0 +1,20 @@
+use std::{os::unix::process::ExitStatusExt, process::ExitStatus};
+
+use bubblewrap;
+
+#[test]
+fn apply_user_namespace() {
+    let args: Vec<String> = vec![
+        "/bin/sh".to_string(),
+        "-c".to_string(),
+        "[ \"$(id -u)\" = \"0\" ] && exit 0 || exit 1".to_string(),
+    ];
+    println!("{:?}", args);
+    let exit_status = bubblewrap::builder::Command::new(args[0].clone().into())
+        .args(args[1..].to_vec().iter().map(|s| s.into()).collect())
+        .exec()
+        .unwrap_or(ExitStatus::from_raw(255 << 8))
+        .code();
+
+    assert_eq!(exit_status.unwrap(), 0);
+}
